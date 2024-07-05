7c029116f539ee705232bae2bbf76d29e25da67d
Longodios <tusaromix@gmail.com>
2024-07-05 09:54:28 +0200

    Creating Component Filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Filters from './Components/Filters'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div className='bg-gray-300 h-screen'>
-       <p>Main View</p>
+       <Filters/>
       </div>
     
     </>
