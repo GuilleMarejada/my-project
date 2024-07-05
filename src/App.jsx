08fa08bb6d574852638833e825eddf204b142994
Longodios <tusaromix@gmail.com>
2024-07-05 15:48:44 +0200

    Web finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./components/Header.jsx"
 import Filters from './Components/Filters'
+import Card from './Components/Card'
+
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const arrayTitulos = ["armarios y cómodas" , "estanterías", "mesas y escritorios", "aparadores", "camas y cabeceros", "mesillas", "sillones y sofás", "lámparas", "sillas y taburetes", "percheros", "marcos y espejos", "otros objetos"]
+  const arrayRecuperados = [29,16,9,8,21,8,8,21,14,19,9,15]
+  const arrayDisponibles = [7,12,2,7,7,6,1,17,3,1,0,8]
+
 
   return (
     <>
-      <div className='bg-gray-300 h-screen'>
-       <p>Main View</p>
+      <div className='bg-gray-300 h-100% px-20'>
+        <div className="flex">
+
+       <Filters/>
+          <div className="grid grid-cols-3 gap-y-4 gap-x-64">
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[0]} recuperados={arrayRecuperados[0]} disponibles={arrayDisponibles[0]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[1]} recuperados={arrayRecuperados[1]} disponibles={arrayDisponibles[1]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[2]} recuperados={arrayRecuperados[2]} disponibles={arrayDisponibles[2]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[3]} recuperados={arrayRecuperados[3]} disponibles={arrayDisponibles[3]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[4]} recuperados={arrayRecuperados[4]} disponibles={arrayDisponibles[4]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[5]} recuperados={arrayRecuperados[5]} disponibles={arrayDisponibles[5]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[6]} recuperados={arrayRecuperados[6]} disponibles={arrayDisponibles[6]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[7]} recuperados={arrayRecuperados[7]} disponibles={arrayDisponibles[7]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[8]} recuperados={arrayRecuperados[8]} disponibles={arrayDisponibles[8]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[9]} recuperados={arrayRecuperados[9]} disponibles={arrayDisponibles[9]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[10]} recuperados={arrayRecuperados[10]} disponibles={arrayDisponibles[10]}/>
+            <Card imagen={"src/assets/muebleAbitacolo.png"} titulo = {arrayTitulos[11]} recuperados={arrayRecuperados[11]} disponibles={arrayDisponibles[11]}/>
+
+          </div>
+        </div>
       </div>
     
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Navbar from "./components/NavBar.jsx";
-import Footer from './components/Footer.jsx';
+import Navbar from "./Components/NavBar.jsx";
+import Footer from './Components/Footer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
