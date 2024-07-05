4db4b9b30f0dda111fff38f4ccd46153c6f18ad8
Longodios <tusaromix@gmail.com>
2024-07-05 15:24:50 +0200

    Filter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Filters from './Components/Filters'
-import Card from './Components/Card'
+
 
 function App() {
   const [count, setCount] = useState(0)
